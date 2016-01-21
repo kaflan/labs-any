3ffcbfb8bb1f0558769f3425195f24537a620cfd
kaflan <kaf_90@mail.ru>
2016-01-21 07:31:41 +0200

    add remove

--- list.js
+++ list.js
@@ -1,94 +1,99 @@
 Node = {
-    create: function(value) {
-        return {
-            value: value,
-            next: null
-        };
-    }
+  create: function(value) {
+    return {
+      value: value,
+      next: null
+    };
+  }
 };
 List = {
-    getLastNode: function(node) {
-         return node.next ? this.getLastNode(node.next) : node;
-    },
-    create: function() {
-        return {
-            _length: 0,
-            head: null
-        };
-    },
-    add: function(list, value) 
+  getLastNode: function(node) {
+    return node.next ? this.getLastNode(node.next) : node;
+  },
+  create: function() {
+    return {
+      _length: 0,
+      head: null
+    };
+  },
+  add: function(list, value) {
+    var node = Node.create(value);
+    if (this.isEmpty(list)) {
+      list.head = node;
+    } else {
+      var last = this.getLastNode(list.head);
+      last.next = node;
+    }
+    list._length++;
+    return node;
+
+  },
+  search: function(list, value) {
+
+    //ищет в списке l значение v и возвращает ссылку на элемент списка с этим значением если оно есть,
+    // в противном случае возвращает null
+    var node = list.head;
+    while (node) {
+      if (node.value === value) break;
+      node = node.next;
+
+    }
+
+    return node;
+
+  },
+  remove: function(list, index) //удаляет из списка l элемент по индексу index (индексация с 0)
     {
-    	var node = Node.create(value);
-        if (this.isEmpty(list)) {
-        	list.head = node;            
-        }
-        else{
-        	var last = this.getLastNode(list.head);
-        	last.next = node;
-        }
-        list._length++;
-        return node;
+      var element = this.get(list, index-1);
+      var next = element.next.next;
+      element.next = next;
+       if(!element){
+          return null;
+       }
+       List._length--;
+      return element;
 
     },
-    search: function(list, value)
+  isEmpty: function(list) {
+    return !list.head;
+  },
+  insertAt: function(list, value, index) //вставляет в список l значение v по индексу i
     {
+      var newNode = Node.create(value);
+      var parentNode = this.get(list, index - 1);
+      newNode.next = parentNode.next;
+      parentNode.next = newNode;
+      list._length++;
+      return parentNode;
+    },
+  toArray: function(list) {
+    var arr = [];
+    var l = list.head;
+    while (l) {
+      arr.push(l.value);
+      l = l.next;
+    }
+    return arr;
+  },
+  size: function(list) //возвращает количество элементов списка
+    {
+      return list._length;
+    },
+  get: function(list, index) //возвращает I-ый элемент списка или false если такой не найден
+    {
+      var node = list.head;
+      // if(index < 0){
+      //   index++;
+      // }
+      if (this.size(list) <= index || this.isEmpty(list) || index < 0) {
+        return false;
+      }
+      for (var i = 0; i !== index; i++) {
 
-        //ищет в списке l значение v и возвращает ссылку на элемент списка с этим значением если оно есть, 
-        // в противном случае возвращает null
-        var node = list.head;
-        // console.log('search',node.value);
-        while(node.value !== value){
-        	node = node.next;
-        	if(!node){
-        		return null;
-        	}
-        }
-        return node;
-       
-    },
-    remove: function(list, index) //удаляет из списка l элемент по индексу index (индексация с 0)
-    {
+        node = node.next;
 
-    },
-    isEmpty: function(list) 
-    {
-        return !list.head;
-    },
-    insertAt: function(list, value, index) //вставляет в список l значение v по индексу i
-    {
-        var newNode = Node.create(value);
-        var parentNode = this.get(list,index-1);
-        newNode.next = parentNode.next;
-        parentNode.next = newNode; 
-        list._length++;
-        return parentNode;
-    },
-    toArray: function(list)
-    {	
-    	var arr = [];
-    	var l = list.head;
-    	while(l){
-    		arr.push(l.value);
-  			l = l.next;
-    	}    	
-    	return arr;
-    },
-    size: function(list) //возвращает количество элементов списка
-    {
-        return list._length;
-    },
-    get: function(list, index) //возвращает I-ый элемент списка или false если такой не найден
-    {
-    	var node = list.head;
-    	if(this.size(list)  <= index|| this.isEmpty(list) || index < 0) {
-    		return false;
-    	}
-    	for(var i =  0 ; i !== index; i++){
-    		 
-    			node = node.next;
-			
-  	 	}
-    	return node;
+      }
+      return node;
     }
 };
 var l = List.create();
@@ -98,10 +103,10 @@ List.add(l, 20);
 // List.add(l, 30);
 
 function print(list) {
-    console.log(list.value);
-    if (list.next) {
-        print(list.next);
-    }
+  console.log(list.value);
+  if (list.next) {
+    print(list.next);
+  }
 }
 
 
@@ -115,4 +120,4 @@ function print(list) {
 // List.insertAt(l, 1, 0);
 // List.insertAt(l, 8, 2);
 print(l.head);
-console.log('list search', List.search(l,2));
+console.log('list remove', List.remove(l, 1));
--- needle3.js
+++ needle3.js
@@ -2,12 +2,12 @@ function search(needle, haystack){
 	var tail = [haystack];
 	var curObj;
 	while(curObj = tail.shift()){
-	for(var in curObj){
+	for(var in curObj) {
 		if(needle === curObj[i]){return true;}
 		if(typeof curObj[i]){
 			tail.push(curObj[i]);
 		}
-	}	
+	}
 	}
 	return false;
 }
